b46e248adcbea80a2d79f4330a51939557609a45
Valentyn Serediuk <serediukit@gmail.com>
2022-11-16 20:55:41 +0200

    adding task

---
+++ test-factorial-master/src/main/java/com/epam/rd/autotasks/FactorialBadInputTesting.java
@@ -0,0 +1,32 @@
+package com.epam.rd.autotasks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FactorialBadInputTesting {
+
+    Factorial factorial = new Factorial();
+
+    @Test
+    void testNullInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial(null));
+    }
+
+    @Test
+    void testNegativeInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("-5"));
+    }
+
+    @Test
+    void testFractionalInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("5.5"));
+    }
+
+    @Test
+    void testNonDigitalInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("abc"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial(""));
+    }
+
+
+}
